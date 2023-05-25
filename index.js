dda60459e78131493ea7fed4fff7706f6fd7edeb
gaurav-uk2001 <gaurav.vaishali2001@gmail.com>
2023-05-26 02:29:20 +0530

    first commit and create showAlltask function

---
+++ index.js
@@ -0,0 +1,62 @@
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
+
+
+const tasks = [];
+
+form.addEventListener("submit",formClick);
+
+function formClick(e){
+    e.preventDefault();
+    
+
+    tasks.push({
+        title:title.value,
+        description:description.value,
+    })
+    showAllTasks();
+}
+
+
+// function clearTasks(){
+//     tasks.forEach(()=>{
+//         const div = document.querySelector(".task");
+//         div.remove();
+//     })
+// }
+
+function showAllTasks(){
+    tasks.forEach(createTaskFunc);   
+}
+
+function createTaskFunc(value,index){
+    // console.log(tasks);
+    
+    const div = document.createElement("div");
+    div.setAttribute("class", "task");
+
+    const innerDiv = document.createElement("div");
+    div.append(innerDiv);
+
+    const p = document.createElement("p");
+    p.innerText = value.title;
+    innerDiv.append(p);
+
+    const span = document.createElement("span");
+    span.innerText = value.description;
+    innerDiv.append(span);
+
+    const btn = document.createElement("button");
+    btn.setAttribute("class", "deleteBtn");
+
+    btn.innerText = "-";
+
+    div.append(btn);
+    container.append(div);
+
+    btn.addEventListener("click",()=>{
+
+    })
+}
